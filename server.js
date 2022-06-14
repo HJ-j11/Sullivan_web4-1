ff580b08868b7fa060bc98559f0422532429bcac
HJ <hyejiku98@gmail.com>
2022-06-14 15:20:44 +0900

    feat: logging and crypto password when signup

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const http = require('http');
 const express = require('express');
-const app = express();
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+const morgan = require('morgan');
+const {logger} = require('./config/logger');
+const session = require('express-session');
+const memoryStore = require('memorystore')(session);
+
+const app = express();
 var port = 3000;
 
 // css, js 경로
@@ -11,6 +15,19 @@ app.use(express.static('public'));
 // bodyParser 이용
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+//session
+app.use(
+    session({
+        secret: "secret key",
+        resave: false,
+        saveUninitialized: true,
+        store : new memoryStore({
+            checkPeriod: 86400000,
+        }),
+        cookie: {maxAge: 86400000},
+    })
+);
 
 // view engines
 app.set("view engine", "html");
@@ -44,21 +61,55 @@ app.get("/new", (req, res) => {
 app.get("/signup", (req,res) => {
     res.render("signup.html");
 })
+
 app.get("/login", (req,res) => {
     res.render("login.html");
 })
+
+app.post("/login", (req, res) => {
+    var userId = req.body.inputId;
+    var password = req.body.inputPassword;
+
+    conn.query(`SELECT USER_ID, UID, PASSWORD, NAME FROM USER_TB WHERE UID=?`,
+    [userId], function(err, data) {
+        if(err) throw err;
+        if(data.length==0) {
+            res.redirect('/');
+        } else {
+            salt = data[0].salt;
+            var pwd = data[0].password;
+            const hashPassword = crypto.createHash('sha512').update(password+salt).digest('hex');
+            if(password === hashPassword) {
+                res.send('<script>alert("로그인 성공했습니다")</script>');
+                res.redirect('/');        
+            } else {
+                res.redirect('/login');
+            }
+        }
+    });
+
+})
+
 
 app.post('/write', (req, res) => {
     var user = req.body.writer;
     var title = req.body.title;
     var contents = req.body.contents;
     var section = req.body.section;
+
+    var session = req.body.session;
+    // 세션 id 체크
+    console.log(`session : ${session}`);
+
     console.log(`${user}, ${contents}, ${section}`);
     
     var sql = `INSERT INTO USER_BOARD (USER_ID, TITLE, CONTENTS, REGDATE) VALUES (?, ?, ?, ?)`;
     
     conn.query(sql, [user, title, contents, new Date()], function(err, data){
-        if(err) throw err;
+        if(err) {
+            res.send('<script>alert("다시 시도해주세요!")</script>');
+            throw err
+        };
         res.redirect('/');
     })
 })
@@ -68,16 +119,29 @@ app.post('/join', (req, res) => {
     var id = req.body.inputId;
     var password = req.body.inputPassword;
     var phone = req.body.inputPhone;
+    var email = req.body.inputEmail;
     
-    const hashPassword = crypto.createHash('sha512').update(password + salt).digest('hex');
-    var checkSql = `SELECT UID FROM USER_TB WHERE UID=${id}`;
-    conn.query(checkSql, function(err, rows) {
-        if(rows.length==0) {
-            var sql = `INSERT INTO USER_TB (USER_NAME, UID, PASSWORD, PHONE, REGDATE) VALUES (?, ?, ?, ?, ?)`;
-            conn.query(sql, [name, id, hashPassword, phone, new Date()], function(err, data){
-                if(err) throw err;
-                res.redirect('/login');
+    const hashPassword = crypto.createHash('sha512' + salt ).update(password).digest('hex');
+    var checkSql = `SELECT UID FROM USER_TB WHERE UID = ?`;
+
+    conn.query(checkSql, [id], function(err, rows) {
+        if(err) {
+            
+        }
+
+        if(rows.length == 0 ) {
+            var sql = `INSERT INTO USER_TB (USER_NAME, UID, PASSWORD, SALT, PHONE, EMAIL, REGDATE) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+            conn.query(sql, [name, id, hashPassword, salt, phone, email, new Date()], function(err, data){
+                if(err) {
+                    res.send('<script>alert("join failed!")</script>');
+                    throw err;
+                }
+                res.send('<script>alert("등록 되었습니다!")</script>');
+                res.redirect('/');
             })
+        } else {
+            res.send('<script>alert("이미 있는 아이디 입니다.")<script>');
+            res.redirect('/login');
         }
     })
 })
